4536eda6814b0d151477ab24d86a10b01bec35b2
zenghui <zenghui>
2020-01-10 10:30:35 +0800

    android buildSrc管理项目

---
+++ buildSrc/src/main/java/Dependencies.kt
@@ -0,0 +1,11 @@
+/**
+ * android buildSrc
+ */
+object Versions {
+    val compileSdkVersion = 29
+    val buildToolsVersion = "29.0.2"
+    val minSdkVersion = 21
+    val targetSdkVersion = 29
+    val versionCode = 1
+    val versionName = "v1.0"
+}
